763dcb04303c27f3fcb555641cb861b9ba4cc3ba
Ashwin Narkar <ashwin.narkar@gmail.com>
2019-04-02 16:41:14 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,13 +7,14 @@ from scipy.io import wavfile
 filename = sys.argv[1]
 fs, data = wavfile.read(filename)
 amplitude = data[:,0]
+print len(amplitude)
 # for i in amplitude:
 # 	print i
 fftOut = fft.rfft(amplitude[0:22049])
 fftMag = absolute(fftOut)
-#for i in fftMag:
-	#print i
-hz = (argmax(fftMag) - 1)/2
+# for i in fftMag:
+# 	print i
+hz = (argmax(fftMag[1:])-1)/2
 print(hz)
 '''
 for i in range(0,44100*3,256):
