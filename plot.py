4ddc1bce27890139ecbea545e214b94389e8c0f1
Ashwin Narkar <ashwin.narkar@gmail.com>
2019-04-09 16:31:45 -0700

    Edge Detection of Notes

--- plot.py
+++ plot.py
@@ -15,12 +15,31 @@ def getHzofChunk(chunk,binsize):
 def plotChunk(chunk):
 	plt.figure(1)
 	plt.plot(chunk)
-	# plt.figure(2)
-	# fftOut = fft.rfft(chunk)
-	# fftMag = absolute(fftOut)
-	# plt.plot(fftMag)
+	plt.figure(2)
+	fftOut = fft.rfft(chunk)
+	fftMag = absolute(fftOut)
+	plt.plot(fftMag)
 	plt.show()
 
+def analyzeRecording(amplitude,chunksize,binsize):
+	i=0
+	MAS=100
+	moving_average = 0
+	edge = False
+	while (i < len(amplitude)-chunksize):
+		moving_average = 0
+		for x in range(MAS):
+			moving_average = moving_average+ absolute(amplitude[x+i])
+		moving_average/=MAS
+		i+=100
+		if (moving_average>1500 and edge==False):
+			print("Edge found at " + str(i) + " Average="+str(moving_average))
+			i+=30
+			freq = getHzofChunk(amplitude[i:i+chunksize],binsize)
+			print(freq)
+			edge=True
+		if (moving_average<500 and edge==True):
+			edge=False
 
 def main():
 	filename = sys.argv[1]
@@ -28,10 +47,11 @@ def main():
 	amplitude = data[:,0]
 	chunksize = 2205
 	binsize = fs/chunksize
-	for i in range(0,len(amplitude),chunksize):
-		note = getHzofChunk(amplitude[i:i+chunksize],binsize)
-		print(note)
-	plotChunk(amplitude)
+	analyzeRecording(amplitude,chunksize,binsize)
+	# for i in range(0,len(amplitude),chunksize):
+	# 	note = getHzofChunk(amplitude[i:i+chunksize],binsize)
+	# 	print(note)
+	#plotChunk(amplitude)
 
 
 if __name__ == "__main__":
