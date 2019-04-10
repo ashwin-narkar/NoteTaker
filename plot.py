d7ac539f19437f57eefcf5b79883751e15b7f5d2
Ashwin Narkar <ashwin.narkar@gmail.com>
2019-04-10 01:54:44 -0700

    Updated plot.py

--- plot.py
+++ plot.py
@@ -15,11 +15,11 @@ def getHzofChunk(chunk,binsize):
 
 def plotChunk(chunk):
 	plt.figure(1)
-	plt.plot(chunk)
-	plt.figure(2)
-	fftOut = fft.rfft(chunk)
-	fftMag = absolute(fftOut)
-	plt.plot(fftMag)
+	plt.plot(absolute(chunk))
+	# plt.figure(2)
+	# fftOut = fft.rfft(chunk)
+	# fftMag = absolute(fftOut)
+	# plt.plot(fftMag)
 	plt.show()
 
 def analyzeRecording(amplitude,chunksize,binsize):
@@ -33,13 +33,13 @@ def analyzeRecording(amplitude,chunksize,binsize):
 			moving_average = moving_average+ absolute(amplitude[x+i])
 		moving_average/=MAS
 		i+=100
-		if (moving_average>1500 and edge==False):
+		if (moving_average>1250 and edge==False):
 			print("Edge found at " + str(i) + " Average="+str(moving_average))
 			i+=30
 			freq = getHzofChunk(amplitude[i:i+chunksize],binsize)
 			print(identifyNote(freq))
 			edge=True
-		if (moving_average<500 and edge==True):
+		if (moving_average<750 and edge==True):
 			edge=False
 
 def main():
@@ -48,11 +48,30 @@ def main():
 	amplitude = data[:,0]
 	chunksize = 2205
 	binsize = fs/chunksize
+	i=0
+	MAS=100
+	moving_average = 0
+	averagePlot = []
+	y = 0
+	while (i < len(amplitude)-chunksize):
+		moving_average = 0
+		for x in range(MAS):
+			moving_average = moving_average+ absolute(amplitude[x+i])
+		moving_average/=MAS
+		averagePlot.append(moving_average)
+		
+		i+=100
+	# plt.figure(1)
+	# plt.plot(absolute(amplitude))
+	# plt.figure(2)
+	# plt.plot(averagePlot)
+	# plt.show()
 	analyzeRecording(amplitude,chunksize,binsize)
 	# for i in range(0,len(amplitude),chunksize):
 	# 	note = getHzofChunk(amplitude[i:i+chunksize],binsize)
 	# 	print(note)
-	#plotChunk(amplitude)
+	# plotChunk(amplitude)
+
 
 
 if __name__ == "__main__":
